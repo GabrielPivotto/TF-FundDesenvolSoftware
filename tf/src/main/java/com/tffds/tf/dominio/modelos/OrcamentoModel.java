edfa8f14f51d13e8264b8f5e187865c07597053e
GabrielPivotto <161343786+GabrielPivotto@users.noreply.github.com>
2025-06-20 18:48:56 +0000

    adicionado algumas coisas que faltavam em OrcamentoModel

--- tf/src/main/java/com/tffds/tf/dominio/modelos/OrcamentoModel.java
+++ tf/src/main/java/com/tffds/tf/dominio/modelos/OrcamentoModel.java
@@ -8,24 +8,31 @@ public class OrcamentoModel {
     private long id;
     private List<ItemPedidoModel> itens;
     private double custoItens;
+    private String pais;
+    private String estado;
+
     /*talvez imposto e desconto devem ser aplicados ao OrcamentoModel usando servicos ao inves
        dele guardar eles pois isso pode causar problema de multiplos atores na classe?        */
     //private double imposto;   
     //private double desconto; 
     private LocalDate dataCriacao;
     private double custoConsumidor;
-    private boolean efetivado;
+    private boolean efetivado;  // sempre checar se é efetivado pois se chegar de
+                                // um repositorio, deve ser declarado como final
 
-    public OrcamentoModel(long id) {
+    public OrcamentoModel(long id, String pais, String estado) {
         this.id = id;
         this.itens = new LinkedList<>();
         this.efetivado = false;
         dataCriacao = LocalDate.now();
     }
 
-    public OrcamentoModel(){
-        this.itens = new LinkedList<>();
-        this.efetivado = false;
+    // constructor que transfere de outros tipos para model
+    public OrcamentoModel(long id, String pais, String estado, List<ItemPedidoModel> itens, boolean efetivado) {
+        this.id = id;
+        this.itens = itens;
+        this.efetivado = efetivado;
+        dataCriacao = LocalDate.now();
     }
 
     public void addItensPedido(PedidoModel pedido){
@@ -37,12 +44,18 @@ public class OrcamentoModel {
     public List<ItemPedidoModel> getItens(){return itens;}
 
     public long getId() {return id;}
-
     public void setId(long id){this.id = id;}
 
     public double getCustoItens() {return custoItens;}
-
     public void setCustoItens(double custoItens){this.custoItens = custoItens;}
+
+    /*retirar setters de pais e estado se a estrategia de deixar as 
+      instancias de orcamento efetivados como final nao funcionar  */
+    public String getPais() {return pais;}
+    public void setPais(String pais) {this.pais = pais;}
+
+    public String getEstado() {return estado;}
+    public void setEstado(String estado) {this.estado = estado;}
 
     //public double getImposto() {return imposto;}
 
