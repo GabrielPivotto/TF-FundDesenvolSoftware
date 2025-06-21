807f9571f7e9b496b1fae1519d5a3b6359baf9aa
PDS-Academico <161345108+PDS-Academico@users.noreply.github.com>
2025-06-21 21:32:32 +0000

    OrcamentoDTO refeito

--- tf/src/main/java/com/tffds/tf/aplicacao/dtos/OrcamentoDTO.java
+++ tf/src/main/java/com/tffds/tf/aplicacao/dtos/OrcamentoDTO.java
@@ -1,6 +1,8 @@
 package com.tffds.tf.aplicacao.dtos;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.tffds.tf.dominio.modelos.ItemPedidoModel;
@@ -10,59 +12,79 @@ public class OrcamentoDTO {
     private long id;
     private List<ItemPedidoDTO> itens;
     private double custoItens;
-    private double imposto;
+    private double impFederal;
+    private double impEstadual;
     private double desconto;
+    private LocalDate dataCriacao;
     private double custoConsumidor;
-    private boolean efetivado;
+    private boolean efetivado;  // sempre checar se é efetivado pois se chegar de
+                                // um repositorio, deve ser declarado como final
 
-    public OrcamentoDTO(long id,List<ItemPedidoDTO> itens,double custoItens,double imposto,double desconto, double custoConsumidor,boolean efetivado) {
+
+    // constructor que transfere de outros tipos para DTO
+    public OrcamentoDTO(long id, List<ItemPedidoDTO> itens, double custoItens, double impFederal, double impEstadual, double desconto, LocalDate dataCriacao, double custoConsumidor, boolean efetivado) {
         this.id = id;
         this.itens = itens;
         this.custoItens = custoItens;
-        this.imposto = imposto;
+        this.impFederal = impFederal;
+        this.impEstadual = impEstadual;
         this.desconto = desconto;
+        this.dataCriacao = dataCriacao;
         this.custoConsumidor = custoConsumidor;
         this.efetivado = efetivado;
     }
 
-    public long getId() {
-        return id;
-    }
+    
 
-    public List<ItemPedidoDTO> getItens(){
-        return itens;
+    public List<ItemPedidoDTO> getItens(){return itens;}
+
+
+    public long getId() {
+        return this.id;
     }
 
     public double getCustoItens() {
-        return custoItens;
+        return this.custoItens;
     }
 
-    public double getImposto() {
-        return imposto;
+    public double getImpFederal() {
+        return this.impFederal;
+    }
+
+    public double getImpEstadual() {
+        return this.impEstadual;
     }
 
     public double getDesconto() {
-        return desconto;
+        return this.desconto;
+    }
+
+    public LocalDate getDataCriacao() {
+        return this.dataCriacao;
     }
 
     public double getCustoConsumidor() {
-        return custoConsumidor;
+        return this.custoConsumidor;
     }
+
 
     public boolean isEfetivado() {
-        return efetivado;
+        return this.efetivado;
     }
 
-    public void efetiva(){
-        efetivado = true;
+    public void setEfetivado(boolean efetivado) {
+        this.efetivado = efetivado;
     }
 
-    public static OrcamentoDTO fromModel(OrcamentoModel orcamento) {
+
+    public OrcamentoDTO fromModel(OrcamentoModel orcamento){
         List<ItemPedidoDTO> itens = new ArrayList<>(orcamento.getItens().size());
         for(ItemPedidoModel ip:orcamento.getItens()){
             itens.add(ItemPedidoDTO.fromModel(ip));
         }
-        return new OrcamentoDTO(orcamento.getId(),itens,orcamento.getCustoItens(),
-                                orcamento.getImposto(),orcamento.getDesconto(),orcamento.getCustoConsumidor(),orcamento.isEfetivado());
+
+        return new OrcamentoDTO(orcamento.getId(), itens, orcamento.getCustoItens(), orcamento.getImpFederal(), orcamento.getImpEstadual(), orcamento.getDesconto(), orcamento.getDataCriacao(), orcamento.getCustoConsumidor(), orcamento.isEfetivado());
     }
+    
 }
+
