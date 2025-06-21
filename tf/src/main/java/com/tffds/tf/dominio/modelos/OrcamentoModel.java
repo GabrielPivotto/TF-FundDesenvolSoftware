94ae12253d383af915bd1b130ce963090a4367fb
PDS-Academico <161345108+PDS-Academico@users.noreply.github.com>
2025-06-21 23:11:07 +0000

    montagem de orcamento

--- tf/src/main/java/com/tffds/tf/dominio/modelos/OrcamentoModel.java
+++ tf/src/main/java/com/tffds/tf/dominio/modelos/OrcamentoModel.java
@@ -1,7 +1,6 @@
 package com.tffds.tf.dominio.modelos;
 
 import java.time.LocalDate;
-import java.util.LinkedList;
 import java.util.List;
 
 public class OrcamentoModel {
--- tf/src/main/java/com/tffds/tf/dominio/servicos/ServicoOrcamento.java
+++ tf/src/main/java/com/tffds/tf/dominio/servicos/ServicoOrcamento.java
@@ -6,9 +6,17 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tffds.tf.dominio.interfaces_persistencia.InterfaceRepItemDeEstoque;
-import com.tffds.tf.dominio.modelos.ItemDeEstoqueModel;
+import com.tffds.tf.dominio.interfaces_persistencia.InterfaceOrcamento;
+import com.tffds.tf.dominio.interfaces_persistencia.InterfaceRepOrcamento;
 import com.tffds.tf.dominio.modelos.ProdutoModel;
+import com.tffds.tf.dominio.modelos.DescontoPadrao;
+import com.tffds.tf.dominio.modelos.OrcamentoModel;
+import com.tffds.tf.dominio.modelos.PedidoModel;
+import com.tffds.tf.dominio.modelos.Imposto;
+import com.tffds.tf.dominio.modelos.ImpostoRS;
+import com.tffds.tf.dominio.modelos.ImpostoSP;
+import com.tffds.tf.dominio.modelos.ImpostoPernambuco;
+import com.tffds.tf.dominio.modelos.ImpostoBrasil;
 
 @Service
 public class ServicoOrcamento{
@@ -17,6 +25,41 @@ public class ServicoOrcamento{
     @Autowired
     public ServicoItemDeEstoque(InterfaceRepOrcamento orcamento) {
         this.orcamento = orcamento;
+    }
+
+    public OrcamentoModel buildOrcamento(PedidoModel pedido, String pais, String estado){
+
+        Imposto fed = null;
+        Imposto est = null;
+
+        switch (pais.toLowerCase()) {
+            case "brasil":
+                fed = new ImpostoBrasil();
+                break;
+        
+            default:
+                return null;
+        }
+
+
+        switch (estado.toLowerCase()) {
+            case "rio grande do sul":
+                est = new ImpostoRS();
+                break;
+
+            case "são paulo":
+                est = new ImpostoSP();
+                break;
+
+            case "pernambuco":
+                est = new ImpostoPernambuco();
+                break;
+                
+            default:
+                return null;
+        }
+
+        return new OrcamentoModel(pedido.getId(), pedido.getItens(), pais, estado, fed, est, new DescontoPadrao());
     }
 
 }
